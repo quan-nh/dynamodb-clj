30c2d89c5b333059047a36bafdaaeedbd190a71c
Quan Nguyen <huu2uan@gmail.com>
2021-04-19 21:48:50 +0700

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dynamodb-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.taoensso/faraday "1.10.1"]
-                 [org.clojure/data.json "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.taoensso/faraday "1.11.1"]
+                 [org.clojure/data.json "2.2.1"]]
   :plugins [[clj-dynamodb-local "0.1.2"]]
   :repl-options {:init-ns dynamodb-clj.core})
